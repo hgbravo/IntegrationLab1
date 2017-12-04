5ec774635436248c81e99149cc5586198938df24
Henry G. Bravo <hgbravo@icloud.com>
2017-12-04 15:52:34 -0500

    First commit with testExample for Jenkins

---
+++ IntegrationLab1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IntegrationLab1
+//
+//  Created by Henry Bravo on 12/4/17.
+//  Copyright © 2017 Henry Bravo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ IntegrationLab1Tests/IntegrationLab1Tests.swift
@@ -0,0 +1,37 @@
+//
+//  IntegrationLab1Tests.swift
+//  IntegrationLab1Tests
+//
+//  Created by Henry Bravo on 12/4/17.
+//  Copyright © 2017 Henry Bravo. All rights reserved.
+//
+
+import XCTest
+@testable import IntegrationLab1
+
+class IntegrationLab1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        XCTAssert(true)
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
